ca937e17c25b8c61ef8cfc0a6103db58422e97e6
Rosinante <rosinante0932@gmail.com>
2025-12-10 19:59:28 +0700

    新站点

---
+++ src/components/RetryButton.vue
@@ -0,0 +1,40 @@
+<template>
+    <!-- 内层：按钮主体 + 实线蓝色描边（inset 1px） -->
+    <a :href="link">
+        <button :class="class">
+            {{ title }}
+        </button>
+    </a>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+const props = defineProps(['title', 'class'])
+
+const link = ref("")
+
+onMounted(() => {
+
+    const href = window.location.href;
+
+    console.log(href, 'href')
+
+    const tag = '?site='
+
+    const tagIndex = href.indexOf(tag);
+
+    let getLink = href.substring(tagIndex + tag.length, href.length);
+
+    const newLinkOrigin = new URL(getLink);
+
+    console.log(newLinkOrigin, 'newLinkOrigin')
+
+    const newLink = `${newLinkOrigin.origin}${newLinkOrigin.hash}`
+
+    console.log(newLink, 'newLink')
+
+    link.value = newLink
+
+})
+
+</script>
