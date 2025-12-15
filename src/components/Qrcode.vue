f640a0c019d8c2c937ca4ae7dc350237149c9264
Rosinante <rosinante0932@gmail.com>
2025-12-15 13:48:08 +0700

    易换服务 关于我们 公共footer

--- src/components/Qrcode.vue
+++ src/components/Qrcode.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="relative" :class="className">
-        <img src="/assets/yhicon.png" alt="下载链接二维码" class="w-[40px] absolute left-[50%] top-[50%] translate-[-50%]">
+        <img src="/assets/yhicon.png" alt="下载链接二维码" class="w-[40px] absolute left-[50%] top-[50%] translate-[-50%]"
+            :style="{ width: `${iconSize}px` }">
         <QrcodeVue :value="text" :size="size" level="Q" />
     </div>
 </template>
@@ -8,7 +9,7 @@
 <script setup lang="ts">
 import QrcodeVue from 'qrcode.vue'
 import { ref } from 'vue'
-defineProps(['size', 'url', 'className'])
+defineProps(['size', 'url', 'className', 'iconSize'])
 const text = ref(`https://example.com?rd=${Math.random()}`) // 扫码内容
 
 
