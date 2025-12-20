02dad84b2c74c6ea026b4224bb428fc85b62fa9a
Rosinante <rosinante0932@gmail.com>
2025-12-20 18:33:27 +0700

    二维码组件优化 layout组件优化

--- src/components/Qrcode.vue
+++ src/components/Qrcode.vue
@@ -2,7 +2,7 @@
     <div class="relative" :class="className">
         <img src="/assets/yhicon.png" alt="下载链接二维码" class="w-[40px] absolute left-[50%] top-[50%] translate-[-50%]"
             :style="{ width: `${iconSize}px` }">
-        <QrcodeVue :value="text" :size="size" level="Q" />
+        <QrcodeVue :value="finalUrl" :size="size" level="Q" />
     </div>
 </template>
 
@@ -11,5 +11,13 @@ import QrcodeVue from 'qrcode.vue'
 import { ref } from 'vue'
 const props = defineProps(['size', 'url', 'className', 'iconSize'])
 
-const text = ref(`${location.origin}${props.url}?rd=${Math.random()}`) // 扫码内容
+const finalUrl = ref(props.url);
+
+onMounted(() => {
+  // 只有浏览器才有 window/location
+  const origin = window.location.origin;
+  if (!/^https?:\/\//i.test(props.url)) {
+    finalUrl.value = origin + props.url;
+  }
+});
 </script>
